a5d78bae3c4f23a558df78d8697956f3ade7bb1b
Baltej Randhawa <baltej.randhawa0@gmail.com>
2022-03-03 16:40:23 -0800

    added routing

--- src/App.js
+++ src/App.js
@@ -1,15 +1,20 @@
 
 import './App.css';
 import Login from './Component/Firebase/Login';
+import {BrowserRouter as Router, Route, Routes} from 'react-router-dom';
 
 function App() {
 
   
 
   return (
-    <div className='main-container'>
-      <Login />
-    </div>
+    <Router>
+      <Routes>
+        <Route path='/' element={<Login />} />
+        {/* <Route path='/:id' element={} */}
+      </Routes>
+
+    </Router>
   );
 }
 
